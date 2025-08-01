6eec6320eff9341ca033c26785a4906295c75264
Arindam Rui Das <153933968+Ori240@users.noreply.github.com>
2025-08-01 10:14:49 +0530

    Add files via upload

---
+++ assets/javascript/main.js
@@ -0,0 +1,7 @@
+let options = {
+    strings: ['a Web Developer', 'a Cyber Security Expert','a Sofware Developer'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    loop: true
+  };
+  let typed = new Typed('.typing', options);
